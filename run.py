5407fb521e74116834a0dbafd3051f76cd6aa9af
HinanawiTS <35885069+HinanawiTS@users.noreply.github.com>
2020-12-12 00:24:56 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
+import sys
+import os
+import pandas as pd
+import json
 
+import matplotlib.pyplot as plt
+from calculator import calculator
+paths = os.getcwd() + "/" 
+sys.path.insert(0, paths + "/src")
+from EDA import eda
+
+def main(targets):
+  
+
+
+
+    if ("test" in targets) or (targets == "test):
+        print(Given )
 if __name__ == '__main__':
     
     targets = sys.argv[1:]
+    
     main(targets)
